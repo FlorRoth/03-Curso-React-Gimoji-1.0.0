3d121f91edb4b271f8b57f220208af1b5e96e1dc
FlorRoth <roth.florencia99@gmail.com>
2023-12-14 13:06:53 -0300

    add hook useLike

---
+++ src/hooks/useLike.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import React,{useState} from 'react';
+
+export const useLike = (initalValue) => {
+
+    const [likes, setLikes] = useState(initalValue);
+
+    const updateLikes = () => {
+        setLikes((prev) => prev+1);
+     }
+
+    
+    return {
+        likes,
+        updateLikes
+    }
+}
